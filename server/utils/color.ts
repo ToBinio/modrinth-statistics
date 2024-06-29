0f47c1592a28835acbaf1fe0f7ba7b906cd12220
tobinio <Tobias.frischmann1@gmail.com>
2024-06-29 08:59:46 +0200

    chore: add missing colors

--- server/utils/color.ts
+++ server/utils/color.ts
@@ -7,27 +7,32 @@ export function getColorForLauncher(name: string): string {
         case 'forge':
             return "#959eef";
         case 'neoforge':
+        case 'optifine':
             return "#f99e6b";
         case 'liteloader':
+        case 'canvas':
             return "#7ab0ee";
+        case 'modloader':
+            return "#6b7280";
+        case 'rift':
+            return "#bbc2d0";
         case 'bukkit':
             return "#f6af7b";
-        case 'bungeecord':
-            return "#d2c080";
         case 'folia':
             return "#a5e388";
         case 'paper':
             return "#eeaaaa";
         case 'purpur':
+        case 'iris':
             return "#c3abf7";
         case 'spigot':
             return "#f1cc84";
-        case 'velocity':
-            return "#83d5ef";
-        case 'waterfall':
-            return "#78a4fb";
         case 'sponge':
             return "#f9e580";
+        case 'vanilla':
+        case 'minecraft':
+        case 'datapack':
+            return "#a5e388";
         default: {
             console.log(`unknown Launcher trying to access color - ${name}`);
             return "#6b7280";
