29e2229f609af3024930ff28d2cf64b00da2b627
tobinio <Tobias.frischmann1@gmail.com>
2024-10-20 11:39:09 +0200

    get rid of installed icons

--- app/pages/charts.vue
+++ app/pages/charts.vue
@@ -72,7 +72,7 @@ onMounted(() => {
 
 <template>
   <div class="flex flex-1">
-    <button class="absolute z-20 px-2 w-48 flex justify-end transition-all" @click="sidebarVisible = !sidebarVisible" :class="{'w-10': !sidebarVisible}">
+    <button class="absolute z-20 px-2 w-48 flex justify-end transition-all" @click="sidebarVisible = !sidebarVisible" :class="{'!w-10': !sidebarVisible}">
       <icon name="ic:round-menu" size="24"/>
     </button>
     <div class="w-48 flex h-full bg-neutral-900 z-10 absolute overflow-scroll transition-all" :class="{'-translate-x-48': !sidebarVisible}">
