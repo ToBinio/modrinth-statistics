0b236351f0a9973ad945f093585ff1f30d35e0ba
tobinio <Tobias.frischmann1@gmail.com>
2025-10-09 14:11:48 +0200

    set additional favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,6 +36,13 @@ export default defineNuxtConfig({
 			link: [
 				{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
 				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "256x256",
+					href: "/favicon.png",
+				},
+				{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+				{
 					rel: "apple-touch-icon",
 					sizes: "180x180",
 					href: "/apple-touch-icon.png",
